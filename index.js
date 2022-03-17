e6f5fd1ff8685bb7bc01c5d71acd402e71da501e
Sandrine <sandrine.thominet@gmail.com>
2022-03-17 16:03:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// appends a cat to the end of the cats array
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+// prepends a cat to the beginning of the cats array
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+// removes the last cat from the cats array
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+// removes the first cat from the cats array
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+// appends a cat to the cats array and returns a new array, leaving the cats array unchanged
+function appendCat(name) {
+    return [ ...cats, name];
+    
+}
+// prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+// removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+function removeLastCat(name) {
+    return cats.slice(0, cats.length - 1);
+}
+
+// removes the first cat from the cats array and returns a new array, leaving the cats array unchanged:
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
